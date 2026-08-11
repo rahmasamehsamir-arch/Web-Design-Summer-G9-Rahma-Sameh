6e53127fd20807a26d0bad6708a701aed66cfede
Rahma <rahmasamehsamir@gmail.com>
2026-08-11 12:19:20 +0300

    task 13

---
+++ Tasks/task 13/script.js
@@ -0,0 +1,14 @@
+
+  function li(element){
+    var item=document.getElementsByClassName("black")
+     for( var i =0; i<item.length; i++){
+        item[i].style.color="black";
+        
+    }
+     
+        element.style.color="red"
+   
+       
+  }
+
+
